e256a8be1ea390fad4a7c4ac1a62a52aa8bd2519
Aser Elshawi <aserhasan@gmail.com>
2023-09-03 16:10:24 -0400

    Adding Call and Ret instructions

--- Led_ON_OFF/Led_ON_OFF/main.asm
+++ Led_ON_OFF/Led_ON_OFF/main.asm
@@ -8,36 +8,37 @@
 
 ; Replace with your application code
 init:
+	 ; INITIALIZATION SP
+	LDI R16,$08
+	OUT $3E,R16
+	LDI R16,$2A
+	OUT $3D,R16
+	; set portA0 direction to output
 	 SBI $1A,0 ; DDRA <- 0b0000-0001  Set as output for PORTA0
 start:
 	SBI $1B,0 ; PORTA <- 0b0000-0001  Set PIN0 in PORTA as 5V
 
-;Creating a loop1 
-LDI R16, 255 ; initialize the register
-loop11:
-;Creating a loop2 
-LDI R17, 255 ; initialize the register
-loop22:
-	DEC R17   ; decrement R16 = R16 - 1 
-	BRNE loop22 ; branch if not equal zero to loop (conditional branching)
-; End of loop2
-	DEC R16   ; decrement R16 = R16 - 1 
-	BRNE loop11 ; branch if not equal zero to loop (conditional branching)
-; End of loop1
-
+	CALL DELAY
 	CBI $1B,0 ;PORTA <- 0b0000-0000  clear PIN0 in PORTA as 0V
+	CALL DELAY
+    rjmp start  ;this is my comment
 
-;Creating a loop1 
-LDI R16, 255 ; initialize the register
+; subroutine
+DELAY:
+	LDI R18, 8
+loop0:
+	
+	LDI R16, 255 ; initialize the register
 loop1:
 ;Creating a loop2 
-LDI R17, 255 ; initialize the register
+	LDI R17, 255 ; initialize the register
 loop2:
 	DEC R17   ; decrement R16 = R16 - 1 
 	BRNE loop2 ; branch if not equal zero to loop (conditional branching)
 ; End of loop2
 	DEC R16   ; decrement R16 = R16 - 1 
 	BRNE loop1 ; branch if not equal zero to loop (conditional branching)
+	DEC R18
+	BRNE loop0;
 ; End of loop1
-    rjmp start  ;this is my comment
-
+	RET
