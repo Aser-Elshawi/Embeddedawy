e73f044787128560d94ac89e2c8d14dd6136e06f
Aser Elshawi <aserhasan@gmail.com>
2023-10-01 12:54:38 -0400

    feat: Enabeling Interrupt0 and Interrupt1 on falling edge with empty main loop

--- Led_ON_OFF/Led_ON_OFF/main.asm
+++ Led_ON_OFF/Led_ON_OFF/main.asm
@@ -5,33 +5,63 @@
 ; Author : asere
 ;
 
+.INCLUDE <m32adef.inc>
 
 ; Replace with your application code
-init:
-	 ; INITIALIZATION SP
-	LDI R16,$04
-	OUT $3E,R16
-	LDI R16,$5F
-	OUT $3D,R16
-	
-main:
-; Load the SP data(address) into the Y register
-	LDI   R29, 0x04
-	LDI   R28, 0x5F
-;  Read Rn Value
-	LDI	  R16, 0x55
-;  Write the Value in the SP pointing address
-	ST	  Y,R16
-; SP -> SP -1 (Increment Stack) Decrement Stack Pointer
-	DEC   R28
-; Load the X register with the absolute address of the SPL  (X -> SPL)
-;	LDI   R27, 0x00
-;	LDI   R26, 0x5D
-; Load R18 -> (X)
-;	ST    X, R28
-	OUT   $3D,R28
-	LDI   R16,33
-	PUSH  R16
-	PUSH  R16
+; Interrupt vector table
+.ORG 0x000
+	JMP init
+	; PD2 interrupt
+	JMP int0_isr
+	; PD3 interrupt
+	JMP int1_isr
 
+.DB 0x55, 0x55
+.ORG 0x050
+; initialize the stack pointer
+init:
+	LDI R16, LOW(RAMEND)
+	OUT SPL,R16
+	LDI R16, HIGH(RAMEND)
+	OUT SPH,R16
+	; intialize PORTB0 as an output
+	SBI DDRB,  0
+	; intialize PORTB1 as an output
+	SBI DDRB,  1
+	; configure the int0 pin PD2 as input
+	CBI	  DDRD,2
+	; configure the int0 pin PD2 as input
+	CBI	  DDRD,3
+	; configure int1 on falling edge, configure int0 on falling edge
+	LDI R16, (1<<ISC11) | (1<<ISC01)
+	OUT MCUCR, R16
+	; set PD2 as pull up
+	SBI   PORTD,2
+	; enable int0 interrupt and enable int1 interrupt
+	LDI   R16, (1<<int0) | (1<<int1)
+	OUT	  GICR, R16
+	; enable global interrupt mask
+	LDI   R16, (1<<SREG_I)
+	OUT	  SREG,R16
+
+main:
+	;add the Blinking LED application
+	JMP	  main
+
+; interrupt service routine
+int0_isr:
+	; execute the interrupt service routine
+	; instructions
+	; instructions
+	SBI PORTB, 0
+	CBI PORTB, 1
+	RETI
+
+int1_isr:
+	; execute the interrupt service routine
+	; instructions
+	; instructions
+	SBI PORTB, 1
+	CBI PORTB, 0
+	RETI
 
